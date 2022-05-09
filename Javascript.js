f4435121ca20adf82fa3ea63a05e53b06adb1058
Norah Taylor <emt368@gmail.com>
2022-05-09 15:10:24 -0400

    first commit

--- Javascript.js
+++ Javascript.js
@@ -1,7 +1,15 @@
-
+// fecth all starship data
+function getStarships() {
     fetch('https://swapi.py4e.com/api/starships')
     .then(res => res.json())
     .then(data => console.log(data))
- 
+}
+getStarships()
 
-//  getStarwars()
+//variables 
+
+
+//build starship info 
+
+
+ 

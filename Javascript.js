f7e43f731b6db045a933fc5ee510b900cb18bede
Corey Schadeck <corey@schadeck.com>
2022-05-12 23:34:23 -0400

    resolved  dblClick event listener stacking issue

--- Javascript.js
+++ Javascript.js
@@ -1,10 +1,9 @@
 // fecth all starship data
 
-    fetch('https://finalspaceapi.com/api/v0/character')
+fetch('https://finalspaceapi.com/api/v0/character')
     .then(res => res.json())
     .then(data => {
         renderData(data)
-        // dblClickEvent(data)
     })
 
 
@@ -18,56 +17,56 @@ const closeButton = document.getElementById("close")
 //elements created 
 const cardImage = document.createElement('img')
 const cardName = document.createElement('h1')
-const cardButton = document.createElement('button')
-const species = document.createElement('p') 
+const species = document.createElement('p')
 const origin = document.createElement('p')
 const dblClick = document.createElement('h5')
 
-cardButton.className = "button"
-
-
-
-//function to iterate through array 
-
-// const renderData = (data) => {
-//    data.forEach()
-// }
-
+// functions
 function renderData(data) {
-    for(const info of data) {
-    const ships = document.createElement('li')
-    
-     ships.innerText = info.name
-     list.appendChild(ships)
-  
+    for (const info of data) {
+        const ships = document.createElement('li')
+        ships.innerText = info.name
+        
         //click event to see the alien details 
-     ships.addEventListener('click', (e) => {
-
-        cardName.textContent = info.name
-         cardImage.src = info.img_url
-         cardImage.classList.add('pic');
-         cardImage.dataset.name = info.name;
-         cardImage.dataset.origin = info.origin;
-    
-         species.textContent = `Species: ${info.species}`
-         origin.textContent = `Origin: ${info.origin}`
-    
-        cardButton.innerHTML = "\"Double click me for a greeting!\""
-        cardButton.style.fontWeight = "bold"
-
-        cardButton.addEventListener('dblclick', (e) => {
-            alert(`Greetings, human. I am ${info.name} and I am from ${info.origin}`)
-         
-            // cardButton.innerHTML = ''
+        ships.addEventListener('click', (e) => {
+            renderCard(info)
+            outerCard.classList.remove('hide');
         })
-         
-         shipCard.append(cardName, cardImage, dblClick, species, origin, cardButton)
-
-        outerCard.classList.remove('hide');
-       
-   })
-   
+        list.appendChild(ships)
     }
+}
+
+function renderCard(info) {
+
+    // clear card
+    shipCard.innerHTML = '';
+    
+    // card name
+    cardName.textContent = info.name
+
+    // card image
+    cardImage.src = info.img_url
+    cardImage.classList.add('pic');
+    cardImage.dataset.name = info.name;
+    cardImage.dataset.origin = info.origin;
+    cardImage.alt = info.name;
+
+    // card species
+    species.textContent = `Species: ${info.species}`
+
+    // card origin
+    origin.textContent = `Origin: ${info.origin}`
+
+    // card button
+    const cardButton = document.createElement('button')
+    cardButton.className = "button"
+    cardButton.innerHTML = "Double click me for a greeting!"
+    cardButton.addEventListener('dblclick', (e) => {
+        alert(`Greetings, human. I am ${info.name} and I am from ${info.origin}`)
+    });
+
+    shipCard.append(cardName, cardImage, species, origin, cardButton)
+
 }
 
 // click event for closeout button
@@ -76,11 +75,11 @@ closeButton.addEventListener('click', (e) => {
 })
 
 
-shipList.addEventListener("mouseover", function( event ) {
+shipList.addEventListener("mouseover", function (event) {
     // highlight the mouseover target
-     event.target.style.color = 'yellow'; 
-  
-    setTimeout(function() {
-      event.target.style.color = "";
+    event.target.style.color = 'yellow';
+
+    setTimeout(function () {
+        event.target.style.color = "";
     }, 1000);
-  }, false);
+}, false);
--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 Corey and Norah's phase-1 project. 
 Basic Story:
 
-We are sharing the details of aliens.  Each card will have detailed information about the alien and a message from the alien.
+We are sharing the details of the characters from Final Space.  Each card will have detailed information about the alien and a message from the alien.
 
 As a user, I want to view the details of aliens so that I can educate myself on the world of aliens. As a user, I will be able to click on the individual alien names to view the alien's name, species and origin. I will be able to doubleclick for a message from the aliens and close out the alien window. 
 
-Features: 3 event listeners: Click on the alien name to see the details of the aliens. Double click on the image to see a personalized message from the aliens. Click on the x to close out the alien window. 
+Features: 3 event listeners: Click on the alien name to see the details of the aliens. Double click on the button at the bottom of the character card to see a personalized message from the character. Click on the x to close out the alien window. 
 
 Array: We will have an array of aliens to iterate through by the for of method to grab the alien information.
 
